4147d1e6135b03a958209b0aa08d3db21b9e8679
YaroslavaKV-11 <jaroslava.schevchuk@gmail.com>
2025-11-06 17:08:56 +0200

    Models

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Tag, Article, Comment
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "icon")
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "publication_date", "is_published")
+    list_filter = ("is_published", "category", "tag")
+    search_fields = ("title", "author", "text")
+    date_hierarchy = "publication_date"
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "article", "publication_date")
+    date_hierarchy = "publication_date"
--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    icon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)  
+    text = models.TextField()
+    image = models.URLField(blank=True)       
+    publication_date = models.DateField()
+    is_published = models.BooleanField(default=True)
+
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name='articles'
+    )
+
+    tag = models.ManyToManyField(Tag, blank=True, related_name='articles')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField(auto_now_add=True)  
+    # один коментар належить одній статті
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='comments'
+    )
+
+    def __str__(self):
+        return f"{self.author}: {self.text[:30]}"
